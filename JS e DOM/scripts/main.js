291c967ff4d93f91bc7387ab14fd12e88c1ed4bf
Lucas Lopes <lucaslopes072013@gmail.com>
2023-11-28 22:23:12 -0300

    Add data validation

--- JS e DOM/scripts/main.js
+++ JS e DOM/scripts/main.js
@@ -15,14 +15,30 @@ function formatDate(date){
     return date.split('-').reverse().join('/');
 }
 
+function validateData(data){
+    let dataIsValid = true;
+
+    // ADD MODAL ERROR TO EVERY VALIDATION
+    if (data.nome.length === 0 || data.dataNascimento.length === 0){
+        dataIsValid = false;
+    }else if (data.nome.length < 3 || 120 <  data.nome.length){
+        dataIsValid = false;
+    }else if (!(/^[a-zA-Z\s]+$/.test(data.nome))){
+        dataIsValid = false;
+    }
+    
+    return dataIsValid;
+}
+
 function showData(event){
     event.preventDefault();
     
     const data = collectData();
     const [nome, dataNascimento] = [data.nomeElement.value, formatDate(data.dataNascimentoElement.value)];
-    const textElement = document.getElementById("print");
 
-    if (nome != '' && dataNascimento != ''){
+    if (validateData({nome, dataNascimento})){
+        const textElement = document.getElementById("print");
+
         textElement.textContent = `${nome} nasceu em ${dataNascimento}`; 
         textElement.style.display = "block";
     }
